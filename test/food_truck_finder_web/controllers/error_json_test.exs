ba393aa0704e91d19d96f41fa8c87a348a7a7b6a
Mackenzie Morgan <macoafi@gmail.com>
2024-10-08 02:54:00 -0400

    formatting

--- test/food_truck_finder_web/controllers/error_json_test.exs
+++ test/food_truck_finder_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule FoodTruckFinderWeb.ErrorJSONTest do
   use FoodTruckFinderWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert FoodTruckFinderWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert FoodTruckFinderWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
